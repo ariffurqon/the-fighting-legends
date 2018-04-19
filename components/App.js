1bf3f895282a47ea580defc9dfc65469365840ea
ariffurqon <arif.furqon@gmail.com>
2018-04-19 16:51:16 -0700

    firsty

---
+++ components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        The Fighting Legends
+      </div>
+    )
+  }
+}
+
+export default App;
